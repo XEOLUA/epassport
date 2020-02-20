eabae6adeea78fa87b39cc119f4d125739894060
Oleg <savitoleg@ukr.net>
2020-02-20 15:17:58 +0200

    1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title','Index')
+
+@section('content')
+{{--    @include('home.main')--}}
+{{--    @include('home.company-info')--}}
+{{--    @include('home.our-values')--}}
+{{--    @include('home.service')--}}
+{{--    @include('home.blog')--}}
+{{--    @include('home.contacts')--}}
+{{--    @include('home.right-side-bar')--}}
+{{--    @include('home.left-side-bar')--}}
+@endsection
