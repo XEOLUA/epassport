d1cfd535de7e23144cde9c866ea0bcfd19545655
Olga <savitskaoo9@ukr.net>
2020-03-29 18:14:37 +0300

    new header+mobi

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,2 +1,6 @@
+//mobile_menu
+$( "#nb_toggle" ).click(function() {
+    $( ".nav_hide" ).toggleClass('hide');
+    $( "#navbarCollapse1" ).toggleClass('show');
 
-console.log('go')
+});
--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -7,7 +7,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
-        <div class="col-xl-6 col-lg-6 col-md-7 navbar-collapse pr-0 pl-0 ml-0 collapse " id="navbarCollapse"  style="margin-left: 10px;">
+        <div class="col-xl-6 col-lg-6 col-md-7 navbar-collapse pr-0 pl-0 ml-0 collapse " id="navbarCollapse1"  style="margin-left: 10px;">
             <ul class="navbar-nav mr-auto my_navbar-nav block_center" >
                 <li class="nav-item active">
                     <a class="nav-link nav-item_big" href="{{route('index')}}">Головна</a>
@@ -30,7 +30,6 @@
                         </li>
                     @endif
                 @endif
-
             </ul>
             <ul class="navbar-nav mr-auto my_navbar-nav nav_hide hide" >
                 @if(!auth()->check())
@@ -40,26 +39,20 @@
                     <li class="nav-item nav-item_big active">
                         <a class="nav-link" href="{{route('login')}}">Вхід</a>
                     </li>
-{{--                @else--}}
-{{--                    <li class="nav-item active">--}}
-{{--                     --}}
-{{--                    </li>--}}
                 @endif
                 @if(Auth::check())
                     <li class="nav-item active user_block">
                         <a class="nav-link nav-item_big " href="{{route('profile')}}">Профіль </a>
-                            <span onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-                                {{--                            {{explode("@",Auth::user()->email)[0]}}[{{ ('Вихід') }}]--}}
-                                <center style="cursor:pointer">
-                                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sign-out-alt" class="svg-inline--fa fa-sign-out-alt fa-w-16 svg_exit" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M497 273L329 441c-15 15-41 4.5-41-17v-96H152c-13.3 0-24-10.7-24-24v-96c0-13.3 10.7-24 24-24h136V88c0-21.4 25.9-32 41-17l168 168c9.3 9.4 9.3 24.6 0 34zM192 436v-40c0-6.6-5.4-12-12-12H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h84c6.6 0 12-5.4 12-12V76c0-6.6-5.4-12-12-12H96c-53 0-96 43-96 96v192c0 53 43 96 96 96h84c6.6 0 12-5.4 12-12z"></path></svg>
-                                </center>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </span>
-
-
+                        <span onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            {{--                            {{explode("@",Auth::user()->email)[0]}}[{{ ('Вихід') }}]--}}
+                            <center style="cursor:pointer">
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sign-out-alt" class="svg-inline--fa fa-sign-out-alt fa-w-16 svg_exit" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M497 273L329 441c-15 15-41 4.5-41-17v-96H152c-13.3 0-24-10.7-24-24v-96c0-13.3 10.7-24 24-24h136V88c0-21.4 25.9-32 41-17l168 168c9.3 9.4 9.3 24.6 0 34zM192 436v-40c0-6.6-5.4-12-12-12H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h84c6.6 0 12-5.4 12-12V76c0-6.6-5.4-12-12-12H96c-53 0-96 43-96 96v192c0 53 43 96 96 96h84c6.6 0 12-5.4 12-12z"></path></svg>
+                            </center>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </span>
                     </li>
                 @endif
             </ul>
@@ -95,7 +88,7 @@
                 @endif
             </ul>
         </div>
-
     </nav>
 </section>
+
 
