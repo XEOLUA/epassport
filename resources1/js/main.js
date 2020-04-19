0f47f03e53bb289b047fb00ad3b46f4533385fc2
Olga <savitskaoo9@ukr.net>
2020-04-19 20:05:21 +0300

    style mainPage

--- resources1/js/main.js
+++
@@ -1,6 +0,0 @@
-//mobile_menu
-$( "#nb_toggle" ).click(function() {
-    $( ".nav_hide" ).toggleClass('hide');
-    $( "#navbarCollapse1" ).toggleClass('show');
-
-});
--- resources1/views/profile.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('title','Profile')
-
-@section('content')
-    @include('home.header')
-    @include('auth.register')
-    @include('home.footer')
-@endsection
-
